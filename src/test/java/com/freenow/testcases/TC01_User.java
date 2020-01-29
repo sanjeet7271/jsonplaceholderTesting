4007f6090e9c9ac9e438c8493c76174fc6c3ca7b
sanjeet7271 <kumarsanjeet141@gamil.com>
2020-01-29 16:43:45 +0530

    logger is added into project

--- src/test/java/com/freenow/testcases/TC01_User.java
+++ src/test/java/com/freenow/testcases/TC01_User.java
@@ -1,18 +1,21 @@
 package com.freenow.testcases;
 
+import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 
+import com.freenow.config.configReader;
 import com.freenow.resources.resourceURLs;
 
 public class TC01_User {
 	resourceURLs resources=new resourceURLs();
+	private static Logger logger = Logger.getLogger(configReader.class);
 	@Test
 	public void readConfigFile() {
 		
-		System.out.println(resources.getBaseURI());
-		System.out.println(resources.getResourceforComments());
-		System.out.println(resources.getResourceforPosts());
-		System.out.println(resources.getResourceforPosts());
+		logger.info(resources.getBaseURI());
+		logger.info(resources.getResourceforComments());
+		logger.info(resources.getResourceforPosts());
+		logger.info(resources.getResourceforPosts());
 		
 	}
 
